f308302db99564a84f2952820331b39a988c5e5d
Candace Pinero <xaverpinero@yahoo.com>
2022-08-05 10:09:19 -0400

    Deleted unnecessary files

--- src/index.js
+++ src/index.js
@@ -6,7 +6,6 @@ import {
 } from "react-router-dom";
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 import Nav from "../src/Components/Nav"
 import About from "./pages/About"
 
@@ -26,7 +25,3 @@ root.render(
 </BrowserRouter>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
