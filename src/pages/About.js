cba9aba2682f1b80aeefcc18ab12d8d00db6f940
candace pinero <candcae.pinero@gmail.com>
2022-09-22 15:19:22 -0400

    working on carousel

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,28 +1,26 @@
 import Grid from "@mui/material/Grid"
+import { Slide } from 'react-slideshow-image';
+import 'react-slideshow-image/dist/styles.css'
 
+const slideImages = [
+    {
+        url: "https://i.imgur.com/zJb3V6K.jpg",
+
+    },
+    {
+
+        url: "https://i.imgur.com/pC9rdDm.png",
+    },
+    {
+
+        url: "https://i.imgur.com/SmIL41N.jpg"
+    }
+]
 const About = () => {
-// let slideIndex = 0;
-// showSlides();
 
-// function showSlides() {
-//   let i;
-//   let slides = document.getElementsByClassName("mySlides");
-//   let dots = document.getElementsByClassName("dot");
-//   for (i = 0; i < slides.length; i++) {
-//     slides[i].style.display = "none";  
-//   }
-//   slideIndex++;
-//   if (slideIndex > slides.length) {slideIndex = 1}    
-//   for (i = 0; i < dots.length; i++) {
-//     dots[i].className = dots[i].className.replace(" active", "");
-//   }
-//   slides[slideIndex-1].style.display = "block";  
-//   dots[slideIndex-1].className += " active";
-//   setTimeout(showSlides, 2000); // Change image every 2 seconds
-// }
-    
-    
-    
+
+
+
     return (
         <Grid container
             direction="row"
@@ -41,35 +39,23 @@ const About = () => {
                 alignItems="baseline"
                 mt={5}
             >
-                {/* slide show here */}
-                {/* <div className="slideshow-container">
-                    <div className="mySlides fade">
-                        <a href="https://www.clickorlando.com/video/community/2022/07/13/veteran-owned-caf-struggling-with-inflation-saved-by-community-in-lake-county/?fbclid=IwAR33b3sD8V6i0vUrrOUMunfE7LVgdhIe1E7XpCLspnxaW_qBMehdiZtbZe4"><img src="https://i.imgur.com/pC9rdDm.png" style={{ width: "100%" }} /></a>
-                        <div className="slideshow-text">slide 1</div>
-                    </div>
-                    <div className="mySlides fade">
-                        <a href="https://www.dailycommercial.com/story/news/local/umatilla/2022/07/12/umatillas-combat-cafe-reopen-after-community-steps-help/10037326002/?fbclid=IwAR17Mvhqiw4C528nTZn8cHo_Kdg-Neh3FPmaB_I5rwxxTcD1dS5WA7wfdWw"><img src="https://i.imgur.com/zJb3V6K.jpg" style={{ width: "100%" }} /></a>
-                        <div className="slideshow-text">slide 2</div>
-                    </div>
-                    <div className="mySlides fade">
-                        <img src="https://i.imgur.com/SmIL41N.jpg" style={{ width: "100%" }} />
-                        <div className="slideshow-text">slide 3</div>
-                    </div>
-                    <br />
-
-                    <div style={{ textAlign: "center" }}>
-                        <span className="dot"></span>
-                        <span className="dot"></span>
-                        <span className="dot"></span>
-                    </div>
-
-                </div> */}
 
             </Grid>
-            <Grid>
-                {/* link to social and contact here */}
-            </Grid>
+
+            <div className="slide-container">
+                <Slide>
+                    {slideImages.map((slideImage, index) => (
+                        <div className="each-slide" key={index}>
+                            <div style={{ 'backgroundImage': `url(${slideImage.url})` }}>
+
+                            </div>
+                        </div>
+                    ))}
+                </Slide>
+            </div>
         </Grid>
+
+
     )
 }
 
